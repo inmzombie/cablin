2e387e4543ae93a67bf6def23bd46a0dd9e2c301
inmzombie <inmzombie@localhost.localdomain>
2014-09-14 19:01:01 -0300

    FOS User security configurado

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,10 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Jhonny\Bundle\AdministradorBundle\AdministradorBundle(),
+            /* Bundles externos */
+            new MWSimple\Bundle\CrudGeneratorBundle\MWSimpleCrudGeneratorBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
