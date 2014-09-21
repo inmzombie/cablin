480106bbdb734bad308658b9a7f8e036874fca57
shonny5 <jonatansilva@gmail.com>
2014-09-21 20:38:18 -0300

    cambios en el login

--- src/Jhonny/Bundle/AdministradorBundle/Entity/Telefono.php
+++ src/Jhonny/Bundle/AdministradorBundle/Entity/Telefono.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
  * Telefono
  *
  * @ORM\Table(name="telefono")
+ * 
  * @ORM\Entity(repositoryClass="Jhonny\Bundle\AdministradorBundle\Entity\TelefonoRepository")
  */
 class Telefono
